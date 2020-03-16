c8b18d937b1066cfc72cbd09e824274d82e4c4fa
arthur12324 <mirumind@gmail.com>
2020-03-16 23:50:33 +0200

    Start create organizer, first prepare styles, material-icons and moment pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+import { SelectorComponent } from './selector/selector.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MomentPipe} from './shared/moment.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrganizerComponent,
+    SelectorComponent,
+    CalendarComponent,
+    MomentPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
